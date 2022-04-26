410fb861e8fa79914eecc4ce80f3cbae163f23c2
i <2687620236@qq.com>
2022-04-26 21:09:45 +0800

    DFP + type(ndarray)->type(matrix)

--- SecondDerivativeMethod.py
+++ SecondDerivativeMethod.py
@@ -30,9 +30,36 @@ def new_town_method(f, init_pos, end_limit, count_limit):
 
 
 def dfp_method(f, init_pos, end_limit, count_limit):
-    search_path = []
+    init_pos = np.array(init_pos)
+    search_path_x = [init_pos.tolist()[0]]
+    search_path_y = [init_pos.tolist()[1]]
 
-    return search_path
+    D = np.identity(len(init_pos))
+    D = np.mat(D)
+    init_pos = np.mat(init_pos)
+    count = 0
+    while count < count_limit:
+        count += 1
+        g = np.array(f.g_list(init_pos.A[0]))
+        d = g * -1
+        d = d.dot(D)
+        fl = Function.LambdaFunction(f, init_pos.A[0], d.A[0])
+        area = OneDimSearch.search_area_get(fl, 1000, 0.25, -5)
+        step = OneDimSearch.golden_partition_search(fl, area, 0.000001)
+        new_pos = init_pos + step * d
+        sk = new_pos - init_pos
+        yk = np.array(f.g_list(new_pos.A[0])) - g
+        yk = np.mat(yk)
+        part1 = (D.dot(yk.T).dot(yk)*D)/(yk.dot(D).dot(yk.T))
+        sk = np.mat(sk)
+        part2 = (sk.T.dot(sk))/(sk.dot(yk.T))
+        D = D - part1 + part2
+        search_path_x.append((new_pos.A[0])[0])
+        search_path_y.append((new_pos.A[0])[1])
+        if np.linalg.norm(new_pos - init_pos) < end_limit:
+            break
+        init_pos = new_pos
+    return search_path_x, search_path_y
 
 
 def bfgs_method(f, init_pos, end_limit, count_limit):
